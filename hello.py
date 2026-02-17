d158d31fb378406fac6c276a2e5f4e6ef60bb1b5
rahulhukeri <rahul.hukeri@whitewolfcapital.com>
2026-02-17 14:06:55 -0500

    Add hourly workflow

--- hello.py
+++ hello.py
@@ -2,4 +2,10 @@ print("hello world")
 a = 10;
 b = 20;
 c = a + b;
+print("This is my first cronjob on github actions");
+print("printing all variables");
+
+print(a);
+print(b);
 print(c);
+print("end of script");
